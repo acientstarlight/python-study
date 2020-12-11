5077a4714406193492079562422a5f4646ed782c
lds <2444539628@qq.com>
2020-12-11 17:38:26 +0800

    if语句多分支

---
+++ 1.Python_Base/12.3If_Multi.py
@@ -0,0 +1,36 @@
+# 分支结构if语句-多分支
+# 根据银行卡余额取不同金额
+"""
+有余额为0的银行卡，客户转账进来不同的金额，这边收到后进行取款操作
+当银行卡转入大于10000时取款8000
+当银行卡转入在6000-10000之间时取款6000
+当银行卡转入在2000-5999之间时取款1500
+当银行卡转入在1000-1999之间时取款800
+当银行卡转入低于1000时不取款直接结束取款
+取款结束后显示输出余额
+"""
+moneyIn = int(input("银行卡转入金额"))
+if moneyIn>10000:
+    moneyOut = 8000
+    moneyLeft = moneyIn-moneyOut
+    print("取款后银行卡余额为",moneyLeft)
+elif 6000<=moneyIn<=10000:
+    moneyOut = 6000
+    moneyLeft = moneyIn-moneyOut
+    print("取款后银行卡余额为", moneyLeft)
+elif 2000<=moneyIn<6000:
+    moneyOut = 1500
+    moneyLeft = moneyIn-moneyOut
+    print("取款后银行卡余额为", moneyLeft)
+elif 1000<=moneyIn<2000:
+    moneyOut = 800
+    moneyLeft = moneyIn - moneyOut
+    print("取款后银行卡余额为", moneyLeft)
+else:
+    print("直接退出取款")
+    moneyLeft = moneyIn
+    print("取款后银行卡余额为", moneyLeft)
+print("结束取款")
+
+
+
