d9e6627aa3b95024c501f1422c261a9801d02802
lds <2444539628@qq.com>
2020-12-31 17:13:52 +0800

    list

--- 1.Python_Base/21.1List-Create.py
+++ 1.Python_Base/21.1List-Create.py
@@ -1,10 +1,11 @@
-# 列表的创建
+# 列表的创建和获取里面元素
 lst = [901,"我的",12.1]
 print(type(lst))
 print(id(lst))
 print(lst)
-lst2 = list([90,"你的",1.1])
-print(type(lst2))
-print(id(lst2))
-print(lst2)
-
+print(lst[2])
+# 获取列表元素的索引（存在相同value的元素，则返回第一个的索引
+lst2 = ["我","我",1,{1,2}]
+print(lst2.index("我"),lst2.index({1,2}))
+# 在指定位置区域寻找指定的元素
+print(lst2.index({1,2},0,4))
