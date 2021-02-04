eb1e23aaa22f9509e78fee31fb16322e893497c8
lds <2444539628@qq.com>
2021-02-04 18:37:08 +0800

    字符串的查询和对齐

---
+++ 1.Python_Base/25.2String_Query.py
@@ -0,0 +1,12 @@
+# 字符串的查询
+
+str1 = "python,python"
+print(str1.index("on"))
+print(str1.rindex("on"))
+print(str1.find("on"))
+print(str1.rfind("on"))
+
+# print(str1.index("ss"))     # 抛valueError
+# print(str1.rindex("ss"))    # 抛valueError
+print(str1.find("ss"))
+print(str1.rfind("ss"))
---
+++ 1.Python_Base/25.3String_Case_Coversion.py
@@ -0,0 +1,9 @@
+# 字符串大小写转换
+str1 = "PYthon"
+print(str1.upper())
+print(str1.lower())
+print(str1.swapcase())
+str2 = "pYTHON fAlse"
+print(str2.capitalize())
+print(str2.title())
+
