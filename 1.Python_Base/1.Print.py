faf46427b907bbdfc4506daeecfd2e3f3487cdbc
李定生 <5399688+ldsldslds@user.noreply.gitee.com>
2021-10-21 15:41:02 +0800

    python修改

--- 1.Python_Base/1.Print.py
+++ 1.Python_Base/1.Print.py
@@ -8,10 +8,12 @@ print("hello world")
 print(2+3)
 
 # 3.输出数据字符串“hello world”到位于D盘根目录new.text中（原来不存在该文件）
-data=open("D://new.text","a+")
-print("hello world",file=data)
+# data=open("D://new.text","a+")
+# print("hello world",file=data)
+data2 = 2
+print("ss: "+str(data2))
 # 关闭该文件
-data.close()
+# data.close()
 
 # 4.输出数据hello, world,nice  要求在一行
 print("hello","world","nice")
